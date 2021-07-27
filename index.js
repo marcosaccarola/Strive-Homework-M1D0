123ff549eb4d32ab6b9a3226f890cc5987071367
marcosaccarola <marco.sacca.ads@gmail.com>
2021-07-27 18:20:54 +0200

    starting

---
+++ index.js
@@ -0,0 +1,7 @@
+// JavaScript basic [150 exercises with solution]
+// source: https://www.w3resource.com/javascript-exercises/javascript-basic-exercises.php
+
+// 1. Write a JavaScript program to display the current day and time in the following format.
+// Sample Output : Today is : Tuesday.
+// Current time is : 10 PM : 30 : 38
+
