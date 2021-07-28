69f4188a598ea31765ab93488ee4efcd334c6c2d
marcosaccarola <marco.sacca.ads@gmail.com>
2021-07-28 13:13:24 +0200

    ex.1 done

--- index.js
+++ index.js
@@ -17,17 +17,23 @@ function ex1(){
             weekday[5] = "Friday"
             weekday[6] = "Saturday"
             var n = weekday[d.getDay()];
-            document.getElementById("ex1Day").innerHTML = "Today is : " + n 
+            document.getElementById("ex1Day").innerHTML = "Today is : " + n + "."
         }
         function whatTime() {    
             var h = d.getHours();
-            var m = d.getMinutes(); 
+            var ampm = h >= 12 ? " PM" : " AM";
+            h = h % 12;
+            h = h ? h : 12; // the hour '0' should be '12'
+            var m = d.getMinutes();
+            m = m < 10 ? "0"+m : m;
             var s = d.getSeconds();
-            document.getElementById("ex1Time").innerHTML = "Current time is : " + h + " : " + m + " : " + s
+            s = s < 10 ? "0"+s : s;
+            document.getElementById("ex1Time").innerHTML = "Current time is : " + h + ampm + " : " + m + " : " + s
         }
     todayIs();
     whatTime();
 }
+
 
 
 
