40bc757b4354230703fef6a115de28c69c699825
marcosaccarola <marco.sacca.ads@gmail.com>
2021-07-28 11:13:12 +0200

    starting first Ex

--- index.js
+++ index.js
@@ -5,3 +5,18 @@
 // Sample Output : Today is : Tuesday.
 // Current time is : 10 PM : 30 : 38
 
+
+var d = new Date();
+function todayIs(){
+    var weekday = Array(7);
+    weekday[0] = "Sunday"
+    weekday[1] = "Monday"
+    weekday[2] = "Tuesday"
+    weekday[3] = "Wednesday"
+    weekday[4] = ""
+    weekday[5] = 
+    weekday[6] =
+    var n = d.getDay();
+    return n
+}
+console.log(todayIs())
