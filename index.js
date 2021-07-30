b2e91f28bfd4f0d0a1ba83e7ccb1aa44518c28b3
marcosaccarola <marco.sacca.ads@gmail.com>
2021-07-30 11:38:54 +0200

    ex8 commitment

--- index.js
+++ index.js
@@ -111,4 +111,4 @@ for (var year = 2014; year <= 2050; year++)
         console.log("1st January is being a Sunday  "+year);
     };
 
-
+// 8. Write a JavaScript program where the program takes a random integer between 1 to 10, the user is then prompted to input a guess number. If the user input matches with guess number, the program will display a message "Good Work" otherwise display a message "Not matched". 
