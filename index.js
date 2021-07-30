13ad7425cf76b8179619ef54db54d8c7d67e4b79
marcosaccarola <marco.sacca.ads@gmail.com>
2021-07-30 20:21:12 +0200

    ex22 commitment

--- index.js
+++ index.js
@@ -231,3 +231,8 @@ function ex21(){
     var c=b==="Py"?c=a:c="Py"+a;
     document.getElementById("ex21return").innerHTML=c
 };
+
+// 22. Write a JavaScript program to remove a character at the specified position of a given string and return the new string.
+function ex22(){
+
+};
