f4c22dec38e6ac7aeda95f0f8f0fea8ff89fb7b8
marcosaccarola <marco.sacca.ads@gmail.com>
2021-07-30 19:04:44 +0200

    ex17 commitment

--- index.js
+++ index.js
@@ -192,3 +192,8 @@ function ex16(){
     var c=a===b?3*(a+b):a+b;
     document.getElementById("ex16return").innerHTML=c;
 };
+
+// 17. Write a JavaScript program to compute the absolute difference between a specified number and 19. Returns triple their absolute difference if the specified number is greater than 19.
+function ex17(){
+
+};
