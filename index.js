f53900b4ef6265b93493024595b5a912da2da190
marcosaccarola <marco.sacca.ads@gmail.com>
2021-07-31 16:32:06 +0200

    ex24 commitment

--- index.js
+++ index.js
@@ -249,3 +249,8 @@ function ex23() {
     console.log(b);
     document.getElementById("ex23return").innerHTML = b;
 };
+
+// 24. Write a JavaScript program to create a new string from a given string with the first character of the given string added at the front and back.
+function ex24(){
+    
+};
