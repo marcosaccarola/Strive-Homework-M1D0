7f0ab86dc098ebfab7c3bbfa162bf670adf6ff8c
marcosaccarola <marco.sacca.ads@gmail.com>
2021-07-31 17:08:48 +0200

    ex30 commitment

--- index.js
+++ index.js
@@ -295,3 +295,8 @@ function ex29(){
     var d=((c>=50&&99>=c)||(b>=50&&99>=b)||(a>=50&&99>=a))?true:false;
     document.getElementById("ex29return").innerHTML=d;
 };
+
+// 30. Write a JavaScript program to check whether a string "Script" presents at 5th (index 4) position in a given string, if "Script" presents in the string return the string without "Script" otherwise return the original one.
+function ex30(){
+    
+};
