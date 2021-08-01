a1b4fe13fbc139ecb036eb32362e17069facbe0d
marcosaccarola <marco.sacca.ads@gmail.com>
2021-08-01 20:19:15 +0200

    ex42 commitment

--- index.js
+++ index.js
@@ -431,3 +431,9 @@ function ex41(){
     }
     document.getElementById("ex41return").innerHTML=d;
 };
+
+// 42. Write a JavaScript program to check whether three given numbers are increasing in strict mode or in soft mode.
+// Note: Strict mode -> 10, 15, 31 : Soft mode -> 24, 22, 31 or 22, 22, 31
+function ex42(){
+    
+};
