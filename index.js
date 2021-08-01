ee40a7cabf768b9a6de724064719d2104722fbd5
marcosaccarola <marco.sacca.ads@gmail.com>
2021-08-01 19:37:12 +0200

    ex38 commitment

--- index.js
+++ index.js
@@ -387,5 +387,9 @@ function ex37(){
             b=b.toLowerCase();
         }
     document.getElementById("ex37result").innerHTML=b;
+};
 
+// 38. Write a JavaScript program to check the total marks of a student in various examinations. The student will get A+ grade if the total marks are in the range 89..100 inclusive, if the examination is "Final-exam." the student will get A+ grade and total marks must be greater than or equal to 90. Return true if the student get A+ grade or false otherwise.
+function ex38(){
+    
 };
